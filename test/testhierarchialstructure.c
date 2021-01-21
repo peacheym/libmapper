c903b92b1ca08b3d6de0020365fd388c4c9115c5
Matt Peachey <peacheym@dal.ca>
2021-01-21 14:38:37 -0400

    Add empty test file and add that test to makefile.am

---
+++ test/testhierarchialstructure.c
@@ -0,0 +1,14 @@
+#include <mapper/mapper.h>
+#include <stdio.h>
+#include <math.h>
+#include <lo/lo.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+    printf("Hello test world\n");
+    return 0;
+}
